2fc48edc52f9cd7d152c818e3e05a2e7ec75acb4
Dakshesh <daksheshbhatnagar@gmail.com>
2025-09-06 18:03:57 +0530

    new

--- src/types/d3-tube-map.d.ts
+++
@@ -1,9 +0,0 @@
-// declare module "d3-tube-map" {
-//   export interface TubeMap {
-//     (selection: any): void;
-//     width(n: number): TubeMap;
-//     height(n: number): TubeMap;
-//     margin(m: { top: number; right: number; bottom: number; left: number }): TubeMap;
-//   }
-//   export function tubeMap(): TubeMap; // named export
-// }
